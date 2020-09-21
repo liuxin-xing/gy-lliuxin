4b9d8ef4cd3ec58afbd5662a6a7e1a69a4d5c4b3
liuxin-xing <1499506626@qq.com>
2020-09-21 14:51:44 +0800

    果芽

---
+++ hello_world.py
@@ -0,0 +1,157 @@
+# l=[1,2,3,4,5]
+# print(l)
+#
+# t=(1,2,3,4,5)
+# print(t[2])
+#
+# a=10
+# b="20"
+# print(a + int(b))
+# print(str(a)+b)
+#
+#
+#
+# a=10
+# b="2.9"
+# print(a+float(b))
+#
+#
+#
+#
+# l=[1,2,3,4,5]
+# print(tuple(l))
+#
+# t=(1,2,3,4,5,6)
+# print(list(t))
+#
+# s=(1,2,3,4,5)
+# print(list(s))
+
+
+# a="liuxin"
+# l = ["a",5,"f"]
+# t=("5","b","f")
+# s={"a","b","f"}
+# d = {"name":"酷酷酷","sex":"搜索"}
+# print("a" in  a
+#     prin
+
+
+
+
+
+#  money = 50000
+# if(money < 5000):
+#   print("上班")
+#  elif (money < 50000):
+#     print("不上班") elif (money < 500000):    print("不
+#
+
+# def div (a,b):
+#     if b == 0 :
+#         print("除数不能为0")
+#     else:
+#         print(a/b)
+# div(50,2)
+# div(10,0)
+
+# def select_data(sql):
+#     res="查询的结果"
+#     return res
+#
+#
+# result=select_data("")
+#
+# print(result)
+#
+# def s(a,b):
+#     return a + b
+# print(s(1,8))
+#
+#
+# def s(a=5,b=9):
+#     return a+b
+# print(s(5,9))
+# print(s(b=20))
+# print(s(10,b=20))
+#
+# def ar(*ages,**kwages):
+#     print(ages)
+#     print(kwages)
+# ar(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,156)
+# ar(a=10,b=12,c=15)
+#
+# a=1000
+# print(id(a))
+# i=print("输入一个数据")
+# print(i)
+#
+#
+# for i in range(1,10):
+#     for j in range(1,i+1):
+#         print(i,"x",j,'=',i*j,end='\t');
+#     print()
+#
+#
+# c=10
+#
+# def aaa():
+#     global c
+#     c=20
+# aaa()
+# print(c)
+#
+#
+#
+# a="1234567890"
+# b="1234"
+#
+# print(a+b)
+# print(a*3)
+# print(a[0])
+# print(a[3])
+# print(a[1:8])
+# print(a[-4:])
+#
+# l=[1,2,3,4,6,7]
+# print(1)
+#
+# a=" afassadasdv "
+# a=a.strip()
+# print(a.strip())
+# print(a.lstrip())
+# print(a.rstrip())
+# line="张三，李四，小五，小四"
+# line=line.repliace(",",",")
+# print(line)
+# print(line.split(','))
+# for i in range(1,10):
+#     for j in range(1,i+1):
+#         print("{}x{}= {}".format(i,j,i*j),end='\t');
+#     print()
+#
+#
+# d = {"name":"小明","age":"20","sex":"男"}
+#
+# print(d["name"]) # 访问字典值
+# d["name"] = "小红" # 修改value
+# d["sex"] = "女" # 因为sex在原字典中不存在，所以是新增
+# d.pop("sex") # 删除字典中的数据
+#
+# d2 = {"sex":"女"}
+# d.update(d2) # 合并两个字典
+# print(dict(d,addr="上海闵行",phone="1856954125")) # 创建一个新字典，并把数据放进去
+
+
+
+class privateDemo():
+    _a = None
+
+    def set_a(self,value):
+    def get_a(self):
+
+
+import  requests
+
+
+ import random
---
+++ 练习题.py
@@ -0,0 +1,55 @@
+import random
+
+
+
+
+
+# import random
+# a1=['张','金','李','王','赵']
+# a2=['玉','明','龙','芳','军','玲']
+# a3=['立','玲','','国',]
+# print()
+# for i in range(1):
+#     name=random.choice(a1)+random.choice(a2)+random.choice(a3)
+# print(name)
+
+# def s():
+#     import random
+#     a = random.choices("0123456789",k=8)
+#     return print(('134')+''.join(a))
+# s()
+#
+# def s(q,w):
+#     import random
+#     a = random.choices(q,k=w)
+#     print(''.join(a))
+# s("rerueyu65468",6)
+
+
+import random
+
+#
+# def phone():
+#     hao_duan_list = ["131",'185','181','183','182','184','186','187','188','189',"131","130","132","133","134","135","136"]
+#     hao_duan = random.choice(hao_duan_list)
+#     res = random.choices("0123456789",k=8)
+#     ba_wei = "".join(res)
+#     return hao_duan + ba_wei
+#
+# print(phone())
+
+
+print()
+try:
+    r = open("b.txt","r")
+except (Exception) as e :
+    print(e)
+    print("程序遇到了问题")
+
+except ZeroDivisionError as e:
+    print("重新打开文件")
+else:
+    print("程序运行没报错")
+finally:
+    print("不管程序有没有报错都会运行")
+    print("打开文件")
